9fe70906468874ea8314ff1deb30bcc29616498c
nugrohoaziz <azizagusn17@gmail.com>
2022-05-18 10:25:06 +0700

    learn hello univers and comment

--- src/main/kotlin/HellowUnivers.kt
+++ src/main/kotlin/HellowUnivers.kt
@@ -1,7 +1,20 @@
 //main function garis miring ini disebut dengan baris komentar atua single line comment
 fun main() {
     println("Hellow Univers")
+
+    val name = "AZiz"
+
+    print("Hello my name is ")
+    println(name)
+    print(if (true) "Always true" else "Always false")
 }
 /*
     ini di sebut dengan multi line comment
  */
+
+/*
+   output:
+       Hello my name is Alfian
+       Always true
+*/
+
