a094023b54a089d56f4c5833847f7618b127f8e2
nugrohoaziz <azizagusn17@gmail.com>
2022-05-18 10:17:14 +0700

    commit init

---
+++ src/main/kotlin/HellowUnivers.kt
@@ -0,0 +1,7 @@
+//main function garis miring ini disebut dengan baris komentar atua single line comment
+fun main() {
+    println("Hellow Univers")
+}
+/*
+    ini di sebut dengan multi line comment
+ */
