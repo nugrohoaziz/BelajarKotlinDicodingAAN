b31f6c466b67f65da55c43cbe5aa31765ccad7f2
nugrohoaziz <azizagusn17@gmail.com>
2022-05-18 14:02:23 +0700

    learn hello univers and data type

---
+++ src/main/kotlin/TipeDataChar.kt
@@ -0,0 +1,8 @@
+fun main() {
+    //Tipe data char ditandai dengan kutip satu
+    var vocal = 'A'
+    println("Vokal" + vocal++)
+    println("Vokal" + vocal++)
+    println("Vokal" + vocal++)
+    println("Vokal" + vocal++)
+}
