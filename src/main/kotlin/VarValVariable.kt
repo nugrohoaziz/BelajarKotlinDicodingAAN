7df8a2795fefd0a2daa27969bcc7e1820682ad9a
nugrohoaziz <azizagusn17@gmail.com>
2022-05-18 11:36:53 +0700

    learn hello univers and data type

---
+++ src/main/kotlin/VarValVariable.kt
@@ -0,0 +1,25 @@
+fun main() {
+    // var adalah jenis varible yang dapat di ubah nilai datanya disebut sebagai mutable
+    var address = "kota"
+    address = "Cilegon"
+    println("$address")
+
+    // Tipe data String
+    val variableA = "Ada"
+    val variableB = "Tikus"
+    println(variableA + variableB)
+
+    // Tipe data Integer
+    val nilaiA = 10
+    val nilaiB = 90
+    val hasil = nilaiA + nilaiB
+    println(hasil)
+
+    //Tipe data Float
+    val komaA = 1.5F
+    val komaB = 1.3F
+    val hasilKoma = komaA + komaB
+    println(hasilKoma)
+
+    
+}
