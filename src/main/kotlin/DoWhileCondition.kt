d0444e19650fa37cc39a6404c67bbc8ba5a24830
nugrohoaziz <azizagusn17@gmail.com>
2022-05-19 09:46:08 +0700

    learn do while and range

---
+++ src/main/kotlin/DoWhileCondition.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var dihitung = 0
+    do {
+        println(dihitung)
+        dihitung++
+    }while (dihitung <= 9)
+}
---
+++ src/main/kotlin/Range.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val rrange = 1..10
+    if (5 in rrange){
+        println("terdapat angka 5")
+    }
+}
