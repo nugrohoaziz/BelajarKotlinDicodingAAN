7452174ffdb6b1e1637ba9351ed526ecf5c26c56
nugrohoaziz <azizagusn17@gmail.com>
2022-05-19 14:57:39 +0700

    learn map of

---
+++ src/main/kotlin/MapOOff.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val binatang = mapOf(
+        "ikan" to "nila",
+        "kucing" to "harimau",
+        "kaki" to 4
+    )
+    println(binatang)
+
+    val mapKeysss = binatang.keys
+    println(mapKeysss)
+}
