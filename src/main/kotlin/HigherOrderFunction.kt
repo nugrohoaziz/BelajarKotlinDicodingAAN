28e73f189a3eedcb38311786fc02f94e604867dc
nugrohoaziz <azizagusn17@gmail.com>
2022-05-22 07:33:24 +0700

    learn Higher Order Function 2

--- src/main/kotlin/HigherOrderFunction.kt
+++ src/main/kotlin/HigherOrderFunction.kt
@@ -1,6 +1,16 @@
 fun main() {
     val hasil = sum2(4)
     println(hasil)
+
+    printRusultHasil(90, sum6)
 }
 
-var sum2: (Int) -> Int = { valuees1 -> valuees1 + valuees1}
+var sum2: (Int) -> Int = { valuees1 -> valuees1 + valuees1 }
+
+//hight order function
+fun printRusultHasil(valueiu: Int, sum6: (Int) -> Int){
+    val resuttslt = sum6(valueiu)
+    println(resuttslt)
+}
+
+var sum6: (Int) -> Int = { nilai24 -> nilai24 + nilai24 }
