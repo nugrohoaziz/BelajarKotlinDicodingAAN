3a23b8b282f5f676ef823959315bc7d4f03decd8
nugrohoaziz <azizagusn17@gmail.com>
2022-05-23 14:09:19 +0700

    learn membuat calss sederhana getter and setter

--- src/main/kotlin/OOP/Binatang.kt
+++ src/main/kotlin/OOP/Binatang.kt
@@ -1,8 +1,35 @@
 package OOP
 
+class mobil(){
+    var warna = "biru"
+
+    get(){
+        println("fungsi get untuk warna")
+        return field
+    }
+
+    set(value){
+        println("fungsi set untuk warna")
+        field = value
+    }
+}
+
+
 fun main() {
     val burung = MemBuatClass(nama = "perkutut", age = 89, isSmall = true, weight = 30.5)
     println("nama burung ini adalah ${burung.nama} umur ${burung.age} berat burung ini ${burung.weight} badan kecil ${burung.isSmall}")
     burung.eat()
     burung.sleep()
+
+    val rental = mobil()
+    rental.warna = "hitam"
+    println("mobil kijang berwana ${rental.warna}")
+
+
+    val getSet = mobil()
+    println("ini warna asli ${getSet.warna}")
+    getSet.warna = "coklat"
+    println("ini warna setelah terganti")
+
+
 }
