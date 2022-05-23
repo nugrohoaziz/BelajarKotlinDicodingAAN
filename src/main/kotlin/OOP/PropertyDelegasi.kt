82506b5bf63a7ed93ccda2abd54d90c6542ad5c3
nugrohoaziz <azizagusn17@gmail.com>
2022-05-23 15:33:10 +0700

    learn property delegasi

--- src/main/kotlin/OOP/PropertyDelegasi.kt
+++ src/main/kotlin/OOP/PropertyDelegasi.kt
@@ -6,6 +6,13 @@ fun main() {
     val namaBinatang = animalaass()
     namaBinatang.name = "cicak"
     println("ini dalah ${namaBinatang.name}")
+
+    val perkenalNenek = AniAni()
+    perkenalNenek.name = "Siti Vina"
+    perkenalNenek.age  = 90
+    println("halo nama nenek adalah ${perkenalNenek.name}")
+    println("halo nama nenek adalah ${perkenalNenek.age}")
+
 }
 
 class DelegasiName {
@@ -29,3 +36,22 @@ class animalaass{
 
 //diatas delegasi code di atas menggunakan type properti string
 //kita juga bisa menggunakan type data any
+
+class AnyDelegasi{
+    private var values: Any = "Default"
+
+    operator fun getValue(clasRef2: Any?, property2: KProperty<*>): Any{
+        println("Fungsi ini sama seperti getter untuk properti ${property2.name} pada class $clasRef2")
+        return values
+    }
+    operator fun setValue(classRef2: Any, property2: KProperty<*>, newValue2: Any) {
+        println("Nilai ${property2.name} dari: $values akan berubah menjadi $newValue2")
+        values = newValue2
+    }
+}
+
+class AniAni{
+    var name : Any by AnyDelegasi()
+    var age : Any by AnyDelegasi()
+}
+
