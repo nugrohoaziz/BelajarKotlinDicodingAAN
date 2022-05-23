f28a32252d1739029e2991f36b0f9e0eae704ef3
nugrohoaziz <azizagusn17@gmail.com>
2022-05-23 16:05:47 +0700

    learn extensi property

---
+++ src/main/kotlin/OOP/ExtensiPropertii.kt
@@ -0,0 +1,14 @@
+package OOP
+
+fun main() {
+
+    val binatangInfo = Anomil("gajah", 34,89,"")
+    println(binatangInfo.getAnomilInfo)
+
+}
+
+class Anomil(var nama23: String, var beratBadan: Int, var umur: Int, var isMamamal: String)
+
+val Anomil.getAnomilInfo : String
+get() = "nama binatang ini adalah ${this.nama23}, berat bada binatang ini adalah ${this.beratBadan}, umur dari binatang ini adalah ${this.umur}"
+
