fb394486e8c1d6c47b16e6315dc4f6753877bbfc
nugrohoaziz <azizagusn17@gmail.com>
2022-05-23 09:52:58 +0700

    learn memberrefrencesss lanjutaan

--- src/main/kotlin/MemberRefrences.kt
+++ src/main/kotlin/MemberRefrences.kt
@@ -1,7 +1,24 @@
+var pesansms = "Kotlin"
 fun main() {
     val jarakAngkaSepuluh = 1..10
     val mencariAngka = jarakAngkaSepuluh.filter(::caraMencariBilanganGenanp)
     println(mencariAngka)
+
+    //extension
+    val mesinPencari = jarakAngkaSepuluh.filter(Int::howToFindOddNumber)
+    println(mesinPencari)
+
+    //properti reference
+    println(::pesansms.name)
+    println(::pesansms.get())
+    ::pesansms.set("Kotlin Academy")
+    println(::pesansms.get())
 }
 
+//member references for lamda
 fun caraMencariBilanganGenanp(numbe: Int) = numbe % 2 == 0
+
+//extension Function
+fun Int.howToFindOddNumber() = this % 2 == 0
+
+
